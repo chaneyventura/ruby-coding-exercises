bfb0b3fe63ac5f15e0718e495cacdc0f9328f750
chaneyventura <chaneyventura@gmail.com>
2017-01-20 14:52:03 -0700

    solution for immutable array

--- december/24.rb
+++ december/24.rb
@@ -1,6 +1,23 @@
 require 'rspec'
+require 'benchmark'
+
+  arr = Array.new(1_000) {rand 1000}
 
 def new_max array_of_elements
+  max_val = 0
+
+  array_of_elements.each do |i|
+    if i > max_val
+      max_val = i
+    end
+end
+  
+  max_val
+end
+
+Benchmark.bm(10) do |x|
+  x.report('Each: ') {new_max arr}
+  x.report('Sorted: ') {arr.sort.last}
 end
 
 describe 'New Max method' do
