3e14864be2e278d6dccbce3f5eab554c6c4b6890
chaneyventura <chaneyventura@gmail.com>
2017-01-30 10:20:58 -0700

    solutions for jna 2-4

--- january/3.rb
+++ january/3.rb
@@ -2,15 +2,19 @@ require 'rspec'
 
 class String
   def total_words
+    scan(/\w+/).count
   end
 
   def word_list
+    list = Hash.new(0)
+    downcase.scan(/\w+/) { |w| list [w] += 1}
+    list
   end
 end
 
 describe 'Word Reporter' do
   before do
-    @str = "- the quick brown fox / jumped over the lazy fox."
+    @str = "- The quick brown fox / jumped over the lazy fox."
   end
 
   it 'Counts words accurately' do
