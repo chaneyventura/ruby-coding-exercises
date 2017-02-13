011d8affafeeef615e7865401904588f70486d43
chaneyventura <chaneyventura@gmail.com>
2017-02-13 09:54:13 -0700

    Solutions 16-18

--- january/18.rb
+++ january/18.rb
@@ -1,3 +1,19 @@
 def game
+  num = rand 25
+  puts "Guess a number between 0 and 24"
+
+  loop do
+    user_answer = gets.chomp.to_i
+
+    if user_answer == num
+      puts "Woohoo! You got it right."
+      break
+    elsif user_answer > num
+      puts "You guessed too high. Please try again."
+    elsif user_answer < num
+      puts "You guessed too low. Please try again."
+    end
+  end
 end
 
+game
